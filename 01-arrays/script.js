6a293bbdaf33a1b0efab204cdbbe116751118c36
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-02 14:41:18 +0200

    add: exercises 4 and 5 01-arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,8 +29,10 @@ const lastTeacher = teachers.pop();
 const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa");
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
